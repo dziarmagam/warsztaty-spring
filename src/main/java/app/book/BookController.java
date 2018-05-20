7fec5d798eb6984ac2c04126eea812393992e322
Marcin Dziarmaga <165393@edu.p.lodz.pl>
2018-05-20 12:13:27 +0200

    init

---
+++ src/main/java/app/book/BookController.java
@@ -0,0 +1,16 @@
+package app.book;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+
+    @GetMapping
+    String test(){
+        return "test";
+    }
+
+}
